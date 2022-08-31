2e27d40c951808a7fee629a18e3c8fa21efa6788
Jessica Garrido <jessica.garrido.di@gmail.com>
2022-08-31 19:16:47 +0200

    création du dossier middleware et auth + mise en place des futures fonctions dans controllers => sauce

---
+++ controllers/sauce.js
@@ -0,0 +1,31 @@
+const sauce = require("../models/sauce");
+
+//Afficher toutes les sauces
+exports.getAllSauce = (req, res, next) => {
+  
+};
+
+// Afficher une seule sauce
+exports.getOneSauce = (req, res, next) => {
+
+};
+
+// Créer une sauce
+exports.createSauce = (req, res, next) => {
+
+};
+
+// Modifier une sauce
+exports.modifySauce = (req, res, next) => {
+
+};
+
+// Supprimer une sauce
+exports.deleteSauce = (req, res, next) => {
+
+};
+
+// Like/dislike sauce
+exports.likeSauce = (req, res, next) => {
+
+};
